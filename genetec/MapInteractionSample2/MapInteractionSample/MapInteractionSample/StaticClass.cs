ded1482ab4cc88ed8a1e8b1e6b7486bde44018e9
sudoyum21 <ba.samson.lam@gmail.com>
2018-01-20 22:45:32 -0500

    missing files

---
+++ genetec/MapInteractionSample2/MapInteractionSample/MapInteractionSample/StaticClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapInteractionSample
+{
+    static class StaticClass
+    {
+        public static int CountStatic = 1;
+    }
+}
---
+++ genetec/MapInteractionSample2/MapInteractionSample/WpfApp1/BindingProperties.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class BindingProperties
+    {
+        #region Constants
+
+        private static readonly ObservableCollection<Filters> m_fires = new ObservableCollection<Filters>();
+        private static ObservableCollection<Filters> m_filters = new ObservableCollection<Filters>();
+        #endregion
+
+        #region Fields
+
+        private bool m_isMapGeoreferenced;
+        private Guid m_mapId = Guid.Empty;
+
+        #endregion
+
+        #region Properties
+
+
+        #endregion
+
+        #region Constructors
+
+        public BindingProperties()
+        {
+            var thread = new Thread(OnThreadStart)
+            {
+                Name = "Fire provider",
+                IsBackground = true
+            };
+            thread.Start();
+            m_filters = new System.Collections.ObjectModel.ObservableCollection<Filters>()
+            {
+                new Filters("Cat"),
+                new Filters("Dog"),
+                new Filters("T-Shirt")
+            };
+        }
+
+        #endregion
+
+        #region Destructors and Dispose Methods
+
+        public void Dispose()
+        {
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        
+
+        /// <summary>
+        /// Simple thread to generate a fire every 5 second on a random location in montreal.
+        /// </summary>
+        private void OnThreadStart()
+        {
+            Random random = new Random();
+
+            while (true)
+            {
+                if (m_isMapGeoreferenced && m_mapId != Guid.Empty)
+                {
+                    var lat = Convert.ToDouble("45." + random.Next(405052, 682052));
+                    var lon = Convert.ToDouble("-73." + random.Next(486714, 981099));
+
+                }
+
+                Thread.Sleep(5000);
+            }
+        }
+
+        #endregion
+
+
+    }
+}
